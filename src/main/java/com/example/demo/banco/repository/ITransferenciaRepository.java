17cee922ea97cef1c26d822aebea109f76f18a5a
Anthony Guamaninga <anthonysub300@gmail.com>
2022-11-28 19:46:18 -0500

    Taller 9: Ejercicio de Componentes de Negocios

--- src/main/java/com/example/demo/banco/repository/ITransferenciaRepository.java
+++ src/main/java/com/example/demo/banco/repository/ITransferenciaRepository.java
@@ -1,5 +1,16 @@
 package com.example.demo.banco.repository;
 
+import com.example.demo.banco.modelo.Transferencia;
+
 public interface ITransferenciaRepository {
+	
+	
+	// CRUD
+	public Transferencia buscar(Integer id);
+	public void actualizar(Transferencia tansferencia);
+	public void insertar(Transferencia tansferencia);
+	public void borrar(Integer id);
+		
+		
 
 }
